a8867d7624cb6fa5d3cf720eea926c08dc49f631
mridul-joshi <mriduljoshimj12@gmail.com>
2024-09-29 00:52:59 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Value/Value";
+import Contact from "./components/Contact/Contact";
+import GetStarted from "./components/GetStarted/GetStarted";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
@@ -15,6 +18,9 @@ function App() {
       <Companies/>
       <Residencies/>
       <Value/>
+      <Contact/>
+      <GetStarted/>
+      <Footer/>
     </div>
   );
 }
